9329a67eb3d162ad8e3f472e5d4b51ab606dbd10
Olga L <olla.bradford@gmail.com>
2022-03-28 17:45:08 +0300

    Added slick-slider

---
+++ src/js/modules/slider.js
@@ -0,0 +1,45 @@
+import $ from "jquery";
+import slick from "slick-carousel";
+$(".slider__items_flowers").slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 1,
+    touchThreshold: 5,
+    touchMove: false,
+    mobileFirst: true,
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+                arrows: true,
+                touchThreshold: 5,
+                touchMove: false,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: true,
+                slidesToShow: 2,
+                touchThreshold: 5,
+                touchMove: false,
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                arrows: true,
+                slidesToShow: 3,
+                draggable: false,
+            }
+        }
+
+    ]
+});
+$(".slider__items_feedback").slick({
+    dots: true,
+    draggable: false,
+    touchThreshold: 5,
+    touchMove: false,
+    adaptiveHeight: true,
+});
