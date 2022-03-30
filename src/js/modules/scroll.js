fd89e9563f18816582a130e23625f96101c12be6
Olga L <olla.bradford@gmail.com>
2022-03-30 12:07:34 +0300

    Updated scroll function. Speed of scroll depends on where pressing link is on the screen(on the top - slower, on the buttom -faster)

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -1,7 +1,7 @@
 export default function smoothScroll() {
     //Collecting all links that starts with #
     const allLinks = document.querySelectorAll("a[href*='#']");
-    const animationDuration = 600;
+    const animationDuration = 500;
     //Set amount of steps to do while animation duration lasts
     const steps = 60;
 
@@ -18,20 +18,26 @@ export default function smoothScroll() {
             //Coordinate Y for each link (no matter what scrollY is)
             const linkYCoordinate = linkInHtml.getBoundingClientRect().top + window.scrollY;
 
-            //Window Y position for up scroll 
+            //Window Y position when we pressed on the link 
             const positionY = window.scrollY;
 
             //Set interval (iterations for smooth scroll)
             const scroller = setInterval(() => {
-                //how mush to scroll by 1 step when scrolling down
-                const stepScroll = linkYCoordinate / steps;
-
+                /*
+                How mush to scroll by 1 step when scrolling down
+                NOTE: This method adaptive.
+                If link to press stands on the top of the page,
+                then scroller will scroll slower than if it was on the bottom of the page.
+                It's all because the distance to pass while scrolling when link at the top
+                is less than when link is at the bottom
+                */
+                const stepScroll = (linkYCoordinate - positionY) / steps;
                 //if block we need scroll to is lower than link we just cliked on, then
                 if (linkYCoordinate > window.scrollY) {
                     /*
                     if delta Y between link and element more
                     than scroll by 1 step and it's not he end of the pфge,
-                    then do вщцт scroll by 1 step
+                    then do down scroll by 1 step
                     */
                     if ((linkYCoordinate - window.scrollY > stepScroll) && (window.innerHeight + window.scrollY < document.body.scrollHeight)) {
                         window.scrollBy(0, stepScroll);
